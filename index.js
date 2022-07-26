091bf905f3270eaee18f1e9f743d811ebd48ea76
Rondolph S Martin <ronnie@Rondolphs-MacBook-Air.local>
2022-07-26 14:42:03 -0400

    create a api firestore

---
+++ index.js
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const PORT = 3002;
+app.use(express.json());
+
+// put our routes here
+ app.get('/', (req, res) => {
+    res.send('Express is fucking working!!!');
+ });
+
+app.listen(PORT, () =>{
+    console.log(`Listening on http://localhost:${PORT}...`);
+});
